9248c0eae142fb880f6cfba6edb2c31629a20258
KarlD7 <64757701+KarlD7@users.noreply.github.com>
2021-04-08 02:50:14 -0400

    Initial Commit

---
+++ OculusIntro/Assets/Scripts/ButtonInputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInputController : MonoBehaviour
+{
+    public UnityEvent ButtonDownEvent = new UnityEvent();
+    public UnityEvent ButtonUpEvent = new UnityEvent();
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ButtonDownEvent.Invoke();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ButtonUpEvent.Invoke();
+        }
+    }
+}
